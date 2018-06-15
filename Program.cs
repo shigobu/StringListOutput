0e5ead92232b23804c78284b71bf69440e8529da
shigobu <shigobu@shigobu.com>
2018-06-15 12:56:12 +0900

    最初のコミット

---
+++ Program.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class StringListOutput
+{
+
+    /// <summary>
+    /// Linuxのlsコマンドみたいにリスト表示します。
+    /// </summary>
+    /// <param name="list">コレクション</param>
+    private static void WriteList(System.Collections.IEnumerable list)
+    {
+        //文字との隙間
+        const int margin = 2;
+        //文字列の配列に変換
+        List<string> stringList = new List<string>();
+        foreach (var item in list)
+        {
+            stringList.Add(item.ToString());
+        }
+        //横に並べられる数(列)の個数を数える
+        int columnCount = Console.BufferWidth;
+        int stringLength = ZenHanTextCounter.Count(stringList[0]) + margin;
+        int listIndex = 1;
+        while (listIndex < stringList.Count)
+        {
+            int tempColumnCount = 0;
+            bool bufferOver = false;
+            bool indexOver = false;
+            while ( true )
+            {
+                if (stringLength > Console.BufferWidth)
+                {
+                    bufferOver = true;
+                    break;
+                }
+                if (!(tempColumnCount < columnCount))
+                {
+                    break;
+                }
+                if (!(listIndex < stringList.Count))
+                {
+                    indexOver = true;
+                    listIndex = 0;
+                }
+                stringLength += ZenHanTextCounter.Count(stringList[listIndex]) + margin;
+                tempColumnCount++;
+                listIndex++;
+            }
+            if (bufferOver)
+            {
+                tempColumnCount--;
+            }
+            if (listIndex < stringList.Count)
+            {
+                if (tempColumnCount < columnCount)
+                {
+                    listIndex = 0;
+                }
+                else if (indexOver)
+                {
+                    break;
+                }
+                stringLength = ZenHanTextCounter.Count(stringList[listIndex]) + margin;
+                listIndex++;
+            }
+            columnCount = tempColumnCount;
+        }
+        //列が決定
+        //二次元配列へ格納
+        listIndex = 0;
+        //行　列
+        List<List<string>> string2DList = new List<List<string>>();
+        while (listIndex < stringList.Count)
+        {
+            List<string> tempList = new List<string>();
+            for (int i = 0; i < columnCount; i++)
+            {
+                if (listIndex < stringList.Count)
+                {
+                    tempList.Add(stringList[listIndex]);
+                }
+                else
+                {
+                    tempList.Add(null);
+                }
+                listIndex++;
+            }
+            string2DList.Add(tempList);
+        }
+        //各列の最大文字数を導き出す。
+        List<int> columnMaxLen = new List<int>();
+        columnMaxLen.Add(0);//一列目は、必ず先頭から始まる
+        for (int i = 0; i < string2DList[0].Count; i++)
+        {
+            int tempMax = 0;
+            for (int j = 0; j < string2DList.Count; j++)
+            {
+                if (!string.IsNullOrEmpty(string2DList[j][i]))
+                {
+                    int strLen = ZenHanTextCounter.Count(string2DList[j][i]);
+                    if ( strLen > tempMax)
+                    {
+                        tempMax = strLen;
+                    }
+                }
+            }
+            columnMaxLen.Add(columnMaxLen.Last() + tempMax + margin);
+        }
+        //表示
+        foreach (var row in string2DList)
+        {
+            for (int i = 0; i < row.Count; i++)
+            {
+                if (!string.IsNullOrEmpty(row[i]))
+                {
+                    Console.CursorLeft = columnMaxLen[i];
+                    Console.Write(row[i]);
+                }
+            }
+            Console.WriteLine();
+        }
+    }
+}
+
---
+++ ZenHanTextCounter.cs
@@ -0,0 +1,106 @@
+﻿/// <summary>
+/// 全角は二文字、半角は一文字として文字数を数える関数を提供する。
+/// 他、全角・半角を判定する関数を提供する。
+/// 参考URL　https://dobon.net/vb/dotnet/string/ishiragana.html
+/// </summary>
+class ZenHanTextCounter
+{
+    /// <summary>
+    /// 全角を２、半角を１と数える。
+    /// </summary>
+    /// <param name="str">数える文字列</param>
+    /// <returns>全角を２文字、半角を１文字と数える文字数</returns>
+    public static int Count(string str)
+    {
+        int count = 0;
+        foreach (char c in str)
+        {
+            if (IsZenkaku(c))
+            {
+                count += 2;
+            }
+            else
+            {
+                count++;
+            }
+        }
+        return count;
+    }
+    /// <summary>
+    /// 指定した Unicode 文字が、半角かどうかを示します。
+    /// </summary>
+    /// <param name="c">評価する Unicode 文字。</param>
+    /// <returns>c が半角である場合は true 。それ以外は false。</returns>
+    public static bool IsZenkaku(char c)
+    {
+        return IsHiragana(c) || IsFullwidthKatakana(c) || IsKanji(c) || IsFullwidthDigit(c) || IsUpperLatin(c) || IsLowerLatin(c);
+    }
+    /// <summary>
+    /// 指定した Unicode 文字が、ひらがなかどうかを示します。
+    /// </summary>
+    /// <param name="c">評価する Unicode 文字。</param>
+    /// <returns>c がひらがなである場合は true。それ以外の場合は false。</returns>
+    public static bool IsHiragana(char c)
+    {
+        //「ぁ」～「より」までと、「ー」「ダブルハイフン」をひらがなとする
+        return ('\u3041' <= c && c <= '\u309F')
+            || c == '\u30FC' || c == '\u30A0';
+    }
+    /// <summary>
+    /// 指定した Unicode 文字が、全角カタカナかどうかを示します。
+    /// </summary>
+    /// <param name="c">評価する Unicode 文字。</param>
+    /// <returns>c が全角カタカナである場合は true。それ以外の場合は false。</returns>
+    public static bool IsFullwidthKatakana(char c)
+    {
+        //「ァ」から「コト」までと、カタカナフリガナ拡張と、
+        //濁点と半濁点を全角カタカナとする
+        //中点と長音記号も含む
+        return ('\u30A1' <= c && c <= '\u30FF')
+            || ('\u31F0' <= c && c <= '\u31FF')
+            || ('\u3099' <= c && c <= '\u309C');
+    }
+    /// <summary>
+    /// 指定した Unicode 文字が、漢字かどうかを示します。
+    /// </summary>
+    /// <param name="c">評価する Unicode 文字。</param>
+    /// <returns>c が漢字である場合は true。それ以外の場合は false。</returns>
+    public static bool IsKanji(char c)
+    {
+        //CJK統合漢字、CJK互換漢字、CJK統合漢字拡張Aの範囲にあるか調べる
+        return ('\u4E00' <= c && c <= '\u9FCF')
+            || ('\uF900' <= c && c <= '\uFAFF')
+            || ('\u3400' <= c && c <= '\u4DBF');
+    }
+    /// <summary>
+    /// 指定した Unicode 文字が、０ から ９ までの数字かどうかを示します。
+    /// </summary>
+    /// <param name="c">評価する Unicode 文字。</param>
+    /// <returns>c が数字である場合は true。それ以外の場合は false。</returns>
+    public static bool IsFullwidthDigit(char c)
+    {
+        return '０' <= c && c <= '９';
+    }
+    /// <summary>
+    /// 指定した Unicode 文字が、英字の大文字かどうかを示します。
+    /// </summary>
+    /// <param name="c">評価する Unicode 文字。</param>
+    /// <returns>c が英字の大文字である場合は true。
+    /// それ以外の場合は false。</returns>
+    public static bool IsUpperLatin(char c)
+    {
+        //全角英字の大文字の時はTrue
+        return 'Ａ' <= c && c <= 'Ｚ';
+    }
+    /// <summary>
+    /// 指定した Unicode 文字が、英字の小文字かどうかを示します。
+    /// </summary>
+    /// <param name="c">評価する Unicode 文字。</param>
+    /// <returns>c が英字の小文字である場合は true。
+    /// それ以外の場合は false。</returns>
+    public static bool IsLowerLatin(char c)
+    {
+        //全角英字の小文字の時はTrue
+        return 'ａ' <= c && c <= 'ｚ';
+    }
+}
